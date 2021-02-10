2951d870b989e60f2c9e9a3eaacd12dc3ab3a231
pascals <advith.nagappa@gmail.com>
2021-02-10 11:43:40 +0100

    Custom Eviction for expired timestamps

--- src/test/scala/io/nubank/challenge/authorizer/window/TransactionWindowSpec.scala
+++ src/test/scala/io/nubank/challenge/authorizer/window/TransactionWindowSpec.scala
@@ -6,41 +6,66 @@ import org.scalatest.funspec.AnyFunSpec
 import fs2._
 import _root_.io.nubank.challenge.authorizer.external.ExternalDomain.Transaction
 import cats.implicits._
+import com.google.common.cache.CacheBuilder
+import scalacache.{Entry, Mode}
+import scalacache.guava.GuavaCache
+
+import java.time.Clock
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 class TransactionWindowSpec extends AnyFunSpec {
 
-  //val window: Stream[IO, Ref[IO, Resource[IO, TransactionWindow]]] = Stream.eval(Ref[IO].of(TransactionWindow.create(2)))
+  implicit val timer: Timer[IO]                 = IO.timer(ExecutionContext.global)
+  implicit val cs: ContextShift[IO]             = IO.contextShift(ExecutionContext.global)
+  implicit val mode: Mode[IO]                   = scalacache.CatsEffect.modes.async
+  implicit val clock: Clock                     = Clock.systemUTC()
+  val expirationSeconds: FiniteDuration         = 120.seconds
+  val timestampEvictionInterval: FiniteDuration = 10.seconds
 
-  /*  val a = for {
-    win <- Stream.eval(Ref[IO].of(TransactionWindow.create(2)))
-    ref <- Stream.eval(win.get)
+  val build: IO[Ref[IO, GuavaCache[ListBuffer[(Long, Long)]]]] = Ref[IO].of(
+    new GuavaCache(
+      CacheBuilder
+        .newBuilder()
+        .expireAfterWrite(expirationSeconds._1, expirationSeconds._2)
+        .maximumSize(100L)
+        .build[String, Entry[ListBuffer[(Long, Long)]]]
+    )
+  )
 
-  } yield ()*/
-
-  implicit val timer: Timer[IO]     = IO.timer(ExecutionContext.global)
-  implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
-
-  //val transaction: Transaction = Transaction("Nike", 240, 1581256283, System.currentTimeMillis())
-
-  val outer: Stream[IO, Option[Seq[(Long, Long)]]] = Stream
-    .resource(TransactionWindow.create(120.seconds))
-    .flatMap { window =>
-      val a: IO[Option[Seq[(Long, Long)]]] = for {
-        ts          <- IO.pure(System.currentTimeMillis())
-        _           <- IO.delay(println(s"Using ${ts}"))
-        transaction <- IO.pure(Transaction("Nike", 240, 1581256283, ts))
-        put         <- window.put(transaction)
-        get         <- window.get("Nike", 240)
-      } yield get
-
-      val b = Stream.eval(window.evictExpiredTimestamps)
-      Stream.eval(a).concurrently(b)
+  val res: Resource[IO, Ref[IO, GuavaCache[ListBuffer[(Long, Long)]]]] = Resource
+    .makeCase(build) {
+      case (window, _) =>
+        IO.delay {
+          window.get.map(win => {
+            println("cleaning up underlying cache")
+            win.underlying.cleanUp()
+          })
+        }
     }
 
-  outer.evalTap(l => IO.delay(println(l.toString()))).compile.drain.unsafeRunSync()
-  outer.delayBy(20.seconds).evalTap(l => IO.delay(println(l.toString()))).compile.drain.unsafeRunSync()
+  val outer: Stream[IO, Option[Seq[(Long, Long)]]] = Stream.resource(res).flatMap { cache =>
+    val win: TransactionWindow = new TransactionWindow(cache)
 
+    val step: IO[Option[Seq[(Long, Long)]]] = for {
+      tsOne          <- IO.pure(System.currentTimeMillis())
+      _              <- IO.delay(println(s"Using ${tsOne} for first transaction"))
+      transactionOne <- IO.pure(Transaction("Nike", 240, 1581256283, tsOne))
+      putOne         <- win.put(transactionOne)
+      _              <- IO.delay(println(s"First Transaction Success"))
+      _              <- IO.delay(Thread.sleep(20000))
+      tsTwo          <- IO.pure(System.currentTimeMillis())
+      _              <- IO.delay(println(s"Using ${tsTwo} for second transaction "))
+      transactionTwo <- IO.pure(Transaction("Nike", 240, 1581256284, tsTwo))
+      putTwo         <- win.put(transactionOne)
+      _              <- IO.delay(println(s"Second Transaction Success"))
+      get            <- win.get("Nike", 240)
+    } yield get
+
+    val evict = Stream.eval(win.evictExpiredTimestamps(timestampEvictionInterval)).metered(3.seconds).repeatN(20)
+    Stream.eval(step).concurrently(evict)
+  }
+
+  outer.evalTap(l => IO.delay(println(l.toString()))).compile.drain.unsafeRunSync()
 }
