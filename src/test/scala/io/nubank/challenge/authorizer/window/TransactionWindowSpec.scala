c1ee4337ce0a0960f20041cc9b61e9ad1ba3092a
pascals <advith.nagappa@gmail.com>
2021-02-10 20:58:15 +0100

    Invalidate cache on release

--- src/test/scala/io/nubank/challenge/authorizer/window/TransactionWindowSpec.scala
+++ src/test/scala/io/nubank/challenge/authorizer/window/TransactionWindowSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.funspec.AnyFunSpec
 import fs2._
 import _root_.io.nubank.challenge.authorizer.external.ExternalDomain.Transaction
 import cats.implicits._
-import com.google.common.cache.CacheBuilder
+import com.google.common.cache.{Cache, CacheBuilder}
 import org.scalatest.Assertion
 import org.typelevel.log4cats.{Logger, SelfAwareStructuredLogger}
 import org.typelevel.log4cats.slf4j.Slf4jLogger
@@ -25,7 +25,6 @@ class TransactionWindowSpec extends AnyFunSpec {
   implicit val mode: Mode[IO]                        = scalacache.CatsEffect.modes.async
   implicit val clock: Clock                          = Clock.systemUTC()
   val cacheExpirationInterval: FiniteDuration        = 2.minutes
-  val timestampEvictionInterval: FiniteDuration      = 10.seconds
   implicit def logger: SelfAwareStructuredLogger[IO] = Slf4jLogger.getLogger[IO]
 
   val build: IO[Ref[IO, GuavaCache[ListBuffer[(Long, Long)]]]] = Ref[IO].of(
@@ -39,18 +38,68 @@ class TransactionWindowSpec extends AnyFunSpec {
   )
 
   val res: Resource[IO, Ref[IO, GuavaCache[ListBuffer[(Long, Long)]]]] = Resource
-    .makeCase(build) {
-      case (window, _) =>
-        IO.delay {
-          window.get.map(win => {
-            logger.info("Cleaning up underlying cache")
-            win.underlying.cleanUp()
-          })
-        }
+    .make(build) { window =>
+      window.modify(win => {
+        win.underlying.invalidateAll()
+        (win, ())
+      })
     }
 
-  it("The second entry should be expired") {
+  it("Write and read two entries") {
+    val timestampEvictionInterval: FiniteDuration = 10.seconds
+    val basicOperationsStream: Stream[IO, IO[Seq[Option[Seq[(Long, Long)]]]]] = Stream
+      .resource(res)
+      .map { cache =>
+        val win: TransactionWindow = new TransactionWindow(cache)
+        for {
+          tsOne          <- IO.pure(System.currentTimeMillis())
+          _              <- logger.info(s"Using ${tsOne} for first transaction")
+          transactionOne <- IO.pure(Transaction("Nike", 240, 1581256213, tsOne))
+          _              <- win.put(transactionOne)
+          _              <- logger.info(s"First Transaction Success")
+          tsTwo          <- IO.pure(System.currentTimeMillis())
+          _              <- logger.info(s"Using ${tsTwo} for second transaction ")
+          transactionTwo <- IO.pure(Transaction("Addidas", 220, 1581256214, tsTwo))
+          _              <- win.put(transactionTwo)
+          _              <- logger.info(s"Second Transaction Success")
+          entryOne       <- win.get("Nike", 240)
+          entryTwo       <- win.get("Addidas", 220)
+        } yield Vector(entryOne, entryTwo)
+      }
+    val basicOperationsTest = for {
+      seq   <- basicOperationsStream
+      items <- Stream.eval(seq)
+    } yield assert(items.size == 2 && items(0).get.head._1 == 1581256213 && items(1).get.head._1 == 1581256214)
+    basicOperationsTest.compile.drain.unsafeRunSync()
+  }
 
+  it("Write and read two entries with same key") {
+    val timestampEvictionInterval: FiniteDuration = 10.seconds
+    val multiKeyOperationsStream: Stream[IO, IO[Option[Seq[(Long, Long)]]]] = Stream.resource(res).map { cache =>
+      val win: TransactionWindow = new TransactionWindow(cache)
+      for {
+        tsOne          <- IO.pure(System.currentTimeMillis())
+        _              <- logger.info(s"Using ${tsOne} for first transaction")
+        transactionOne <- IO.pure(Transaction("Nike", 240, 1581256223, tsOne))
+        _              <- win.put(transactionOne)
+        _              <- logger.info(s"First Transaction Success")
+        tsTwo          <- IO.pure(System.currentTimeMillis())
+        _              <- logger.info(s"Using ${tsTwo} for second transaction ")
+        transactionTwo <- IO.pure(Transaction("Nike", 240, 1581256224, tsTwo))
+        _              <- win.put(transactionTwo)
+        _              <- logger.info(s"Second Transaction Success")
+        entryOne       <- win.get("Nike", 240)
+      } yield entryOne
+    }
+    val multiKeyOperationsTest = for {
+      value <- multiKeyOperationsStream
+      items <- Stream.eval(value)
+    } yield assert(items.get.size == 2 && (items.get)(0)._1 == 1581256223 && (items.get)(1)._1 == 1581256224)
+    multiKeyOperationsTest.compile.drain.unsafeRunSync()
+  }
+
+  it("Older entry of a key should be expired") {
+    val timestampEvictionInterval: FiniteDuration = 10.seconds
     val timestampExpirationStream: Stream[IO, Option[Seq[(Long, Long)]]] = Stream.resource(res).flatMap { cache =>
       val win: TransactionWindow = new TransactionWindow(cache)
 
@@ -58,13 +107,13 @@ class TransactionWindowSpec extends AnyFunSpec {
         tsOne          <- IO.pure(System.currentTimeMillis())
         _              <- logger.info(s"Using ${tsOne} for first transaction")
         transactionOne <- IO.pure(Transaction("Nike", 240, 1581256283, tsOne))
-        putOne         <- win.put(transactionOne)
+        _              <- win.put(transactionOne)
         _              <- logger.info(s"First Transaction Success")
         _              <- IO.delay(Thread.sleep(20000))
         tsTwo          <- IO.pure(System.currentTimeMillis())
         _              <- logger.info(s"Using ${tsTwo} for second transaction ")
         transactionTwo <- IO.pure(Transaction("Nike", 240, 1581256284, tsTwo))
-        putTwo         <- win.put(transactionTwo)
+        _              <- win.put(transactionTwo)
         _              <- logger.info(s"Second Transaction Success")
         get            <- win.get("Nike", 240)
       } yield get)
@@ -79,7 +128,7 @@ class TransactionWindowSpec extends AnyFunSpec {
     }
     val timestampExpirationTest = for {
       value <- timestampExpirationStream
-    } yield (assert(value.get.size == 1 && value.get.head._1 == 1581256284))
+    } yield assert(value.get.size == 1 && value.get.head._1 == 1581256284)
 
     timestampExpirationTest.compile.drain.unsafeRunSync()
 
