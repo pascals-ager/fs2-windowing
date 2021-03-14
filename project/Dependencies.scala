43770e541c03f9e8fc382750b37894537b1ab911
pascals <advith.nagappa@gmail.com>
2021-03-14 12:16:22 +0100

    remove scalacache deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,6 @@ object Dependencies {
 
   object Versions {
     val fs2 = "2.5.0"
-    val cache = "0.28.0"
     val circeVersion = "0.13.0"
     val scalatest = "3.1.1"
     val log4cats = "1.2.0-RC2"
@@ -15,11 +14,6 @@ object Dependencies {
   lazy val fs2 = Seq(
     "co.fs2" %% "fs2-core" % Versions.fs2,
     "co.fs2" %% "fs2-io" % Versions.fs2
-  )
-
-  lazy val cache = Seq(
-    "com.github.cb372" %% "scalacache-guava" % Versions.cache,
-    "com.github.cb372" %% "scalacache-cats-effect" % Versions.cache
   )
 
   lazy val circe = Seq(
